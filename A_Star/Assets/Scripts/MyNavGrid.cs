c2d4e336d27aba9e2f67fd69ca00ad63a93dc046
Georg Becker <Georg.Becker@gmx.at>
2023-06-01 16:33:37 +0200

    Implement nav grid creation

--- A_Star/Assets/Scripts/MyNavGrid.cs
+++ A_Star/Assets/Scripts/MyNavGrid.cs
@@ -5,24 +5,61 @@ using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 [ExecuteAlways]
 public class MyNavGrid : MonoBehaviour
 {
     [SerializeField] private Vector2Int dimensions = new Vector2Int(10, 10);
-    [SerializeField, Range(0f, 1f)] private float fieldFill = 0.8f;
+    private bool[,] _blocked;
 
+    private void OnDrawGizmos()
+    {
+        
+        CalculateWalkable();
+        for (int i = 0; i < dimensions.y; i++)
+        {
+            for (int j = 0; j < dimensions.x; j++)
+            {
+                if (_blocked[i,j])
+                {
+                    Gizmos.color = new Color(1, 0, 0, 0.5f);
+                }
+                else
+                {
+                    Gizmos.color = new Color(0, 1, 0, 0.5f);
+                }
+                Gizmos.DrawSphere(transform.position +
+                                  new Vector3((transform.localScale.x * (j + 0.5f)) / dimensions.x, 0.25f,
+                                      (transform.localScale.z * (i + 0.5f)) / dimensions.y), 0.2f);
+            }
+        }
+    }
 
-    [SerializeField] private Material material;
-    private MeshFilter _meshFilter;
-    private MeshRenderer _meshRenderer;
-    
     private void Awake()
     {
-        _meshFilter = GetComponent<MeshFilter>();
-        _meshRenderer = GetComponent<MeshRenderer>();
-        _meshFilter.mesh = GenerateGridMesh(dimensions.x, dimensions.y, fieldFill);
-        _meshRenderer.sharedMaterial = material;
+        CalculateWalkable();
+        
+    }
+
+    private void CalculateWalkable()
+    {
+        _blocked = new bool[dimensions.y, dimensions.x];
+        for (int i = 0; i < dimensions.y; i++)
+        {
+            for (int j = 0; j < dimensions.x; j++)
+            {
+                Collider[] colliders = Physics.OverlapBox(transform.position + new Vector3(
+                        transform.localScale.x * (j + 0.5f) / dimensions.x,
+                        0,
+                        transform.localScale.z * (i + 0.5f) / dimensions.y),
+                    new Vector3(
+                        transform.localScale.x / (4*dimensions.x) ,
+                        2,
+                        transform.localScale.z / (4*dimensions.y)
+                    )
+                );
+                if (colliders.Length > 0) _blocked[i,j] = true;
+            }
+        }
     }
 
     void Start()
@@ -33,52 +70,5 @@ public class MyNavGrid : MonoBehaviour
     void Update()
     {
     }
-
-    private Mesh GenerateGridMesh(int columns, int rows, float fieldFill)
-    {
-        Vector3[] vertices = new Vector3[columns * rows * 4];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                vertices[4 * (i * rows + j) + 0] = new Vector3(
-                    j / (float)columns + 1f / columns * fieldFill,
-                    0,
-                    i / (float)rows + 1f / rows * fieldFill
-                );
-                vertices[4 * (i * rows + j) + 1] = new Vector3(
-                    j / (float)columns + 1f / columns * fieldFill,
-                    0,
-                    (i + 1) / (float)rows - 1f / rows * fieldFill
-                );
-                vertices[4 * (i * rows + j) + 2] = new Vector3(
-                    (j + 1) / (float)columns - 1f / columns * fieldFill,
-                    0, 
-                    i / (float)rows + 1f / rows * fieldFill
-                );
-                vertices[4 * (i * rows + j) + 3] = new Vector3(
-                    (j + 1) / (float)columns - 1f/ columns * fieldFill,
-                    0,
-                    (i + 1) / (float)rows - 1f / rows * fieldFill);
-            }
-        }
-        int[] triangles = new int[columns * rows * 6];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                triangles[6 * (i * rows + j) + 0] = 4 * (i * rows + j) + 0;
-                triangles[6 * (i * rows + j) + 1] = 4 * (i * rows + j) + 1;
-                triangles[6 * (i * rows + j) + 2] = 4 * (i * rows + j) + 2;
-                triangles[6 * (i * rows + j) + 3] = 4 * (i * rows + j) + 2;
-                triangles[6 * (i * rows + j) + 4] = 4 * (i * rows + j) + 1;
-                triangles[6 * (i * rows + j) + 5] = 4 * (i * rows + j) + 3;
-            }
-        }
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
-        return mesh;
-    }
+    
 }

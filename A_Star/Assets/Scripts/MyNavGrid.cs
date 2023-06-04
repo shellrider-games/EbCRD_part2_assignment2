4b33e5af97e7067be621161923e6ede7ba9c9e02
Georg Becker <Georg.Becker@gmx.at>
2023-06-04 17:00:21 +0200

    Implements A star

--- A_Star/Assets/Scripts/MyNavGrid.cs
+++ A_Star/Assets/Scripts/MyNavGrid.cs
@@ -1,13 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
+
 [ExecuteAlways]
 public class MyNavGrid : MonoBehaviour
 {
+    private class AStarNode
+    {
+        public Vector2Int Position;
+        public int GCost;
+        [CanBeNull] public AStarNode Parent;
+
+        public AStarNode(Vector2Int position, int gCost, AStarNode parent)
+        {
+            this.Position = position;
+            this.GCost = gCost;
+            this.Parent = parent;
+        }
+    }
+    
     [SerializeField] private Vector2Int dimensions = new Vector2Int(10, 10);
     private bool[,] _blocked;
 
@@ -93,8 +110,84 @@ public class MyNavGrid : MonoBehaviour
 
         return nextField;
     }
-    
-    private List<Vector2Int> GetReachableNeighboursOf(Vector2Int position, List<Vector2Int> closed)
+
+    private AStarNode FindPath(Vector2Int from, Vector2Int target)
+    {
+        List<AStarNode> closed = new List<AStarNode>();
+        List<AStarNode> open = new List<AStarNode>();
+        AStarNode current = new AStarNode(from, 0, null);
+        closed.Add(current);
+        do
+        {
+            List<Vector2Int> neighbours = GetReachableNeighboursOf(current.Position);
+            foreach (Vector2Int position in neighbours)
+            {
+                if(closed.Find(node => node.Position == position) is not null) continue;
+                AStarNode alreadyExists = open.Find(node => node.Position == position);
+                
+                if (alreadyExists is not null)
+                {
+                    if (alreadyExists.Position.x != position.x && alreadyExists.Position.y != position.y)
+                    {
+                        if (current.GCost + 14 < alreadyExists.GCost)
+                        {
+                            alreadyExists.GCost = current.GCost + 14;
+                            alreadyExists.Parent = current;
+                        }
+                    }
+                    else
+                    {
+                        if (current.GCost + 10 < alreadyExists.GCost)
+                        {
+                            alreadyExists.GCost = current.GCost + 10;
+                            alreadyExists.Parent = current;
+                        }
+                    }
+                }
+                else
+                {
+                    if (current.Position.x != position.x && current.Position.y != position.y)
+                    {
+                        open.Add(new AStarNode(position, current.GCost+14, current));
+                    }
+                    else
+                    {
+                        open.Add(new AStarNode(position, current.GCost+10, current));
+                    }
+                }
+
+                AStarNode next = LowestFCost(open, target);
+                if (next.Position == target)
+                {
+                    return next;
+                }
+
+                open.Remove(next);
+                closed.Add(next);
+                current = next;
+            }
+        } while (open.Count > 0);
+
+        return LowestFCost(closed, target);
+    }
+
+    private AStarNode LowestFCost(List<AStarNode> list, Vector2Int target)
+    {
+        if (list.Count == 0) return null;
+        AStarNode lowestFCost = list[0];
+        for (int i = 1; i < list.Count; i++)
+        {
+            if (list[i].GCost + Distance(list[i].Position, target) <
+                lowestFCost.GCost + Distance(lowestFCost.Position, target))
+            {
+                lowestFCost = list[i];
+            }
+        }
+
+        return lowestFCost;
+    }
+
+    private List<Vector2Int> GetReachableNeighboursOf(Vector2Int position)
     {
         List<Vector2Int> reachableNeighbours = new List<Vector2Int>();
         
